c96a777efb263f7762049b6dbca7b84abd9dac68
Daniel Paulse <danielpaulse@gmail.com>
2019-03-30 05:50:26 +0100

    - Renamed ExoticToonRamp.png To ExoticToonRamp01.png - Added ExoticToonRamp02.png - Created Demo(Standard) Scene - Created Demo(Cold) Scene - Created Demo(Warm) Scene - Created Demo(Exotic) Scene - Created Standard Toon Material - Created Cold Toon Material - Created Warm Toon Material - Created Exotic Toon Material - Created Scripts Folder - Created ToonLight.cs Script (Light Direction Component)

---
+++ [Shader] Toon Shader/Assets/Scripts/ToonLight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToonLight : MonoBehaviour
+{
+    private Light light = null;
+
+    void OnEnable()
+    {
+        light = this.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (light != null)
+        {
+            Shader.SetGlobalVector("_ToonShader_LightDirection_Vector4", -this.transform.forward);
+        }
+    }
+}
